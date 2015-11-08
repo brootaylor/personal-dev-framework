c1093c19c3ecf5c35e9c02c23e2cdfa4b47d737b
Broo <brootaylor@gmail.com>
2015-11-08 12:16:17 +0000

    Added Copyright and auto year generation.

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-    // Generates the content for the meta title & description
-    $meta_title = "This will render in the title tag"; 
-    $meta_description = "This will render in the meta description tag";
-    $meta_canonical = "/Page URL goes here for canonical purposes. Not needed on index page.";
+    // Generates the content for the page title, meta title & description
+    $page_title = "This is the page title";
+    $page_title_extra = "| The title's context eg. | Bruce Taylor's Personal Website";
+    $page_description = "This will render in the meta description tag";
+    $page_canonical = "/Page URL goes here for canonical purposes. Not needed on index page. eg. /about-us";
 
     ob_start();
 ?>
@@ -11,7 +12,7 @@
         <!-- Main content -->
         <main role="main" id="main">
 
-            <h1>This is my template</h1>
+            <h1><?php echo $page_title; ?></h1>
 
             <p>Enim turpis enim vel! Parturient mus lectus parturient nec sagittis parturient! Enim in auctor in. In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat. Adipiscing aenean.</p>
             
