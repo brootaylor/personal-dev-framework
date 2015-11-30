07a788dace262259343d6de0b37416904483beb3
Bruce Taylor <btaylor@CZC21815FV.mcr.local>
2015-11-30 11:09:04 +0000

    Extra comments and markup changes.

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     ob_start();
 ?>
 
-        <!-- Main content -->
+        <!-- The <main> element is used to enclose the main content, i.e. that which contains the central topic of a document -->
+        <!-- ARIA: the landmark role "main" is added here as it contains the main content of the document, and it is recommended to add it to the
+        <main> element until user agents implement the required role mapping. -->
         <main role="main" id="main">
 
             <h1><?php echo $page_title; ?></h1>
@@ -18,6 +20,20 @@
             
         </main>
         <!-- / Main content -->
+
+        <!-- An <aside> is used to enclose content that is additional to the main content but not essential. If it were removed, the meaning of the main content should not be lost, but the content of the <aside> also retains its meaning.
+        NOTE: the aside is placed outside of the <main> element as while its content is related to the content that is within the <main>
+        element, it is not part of it -->
+        <!-- ARIA: the landmark role "complementary" is added here as it contains supporting information for the main content that remains meaningful even when separated from it -->
+        <aside role="complementary">
+        
+            <!-- A <header> element is not required here as the heading only contains a single <h3> element --> 
+            <h3>Did you know?</h3>
+            
+            <!-- Content -->
+            <p>The article and section elements cause a lot of confusion when people are trying to decide how and when to use them. The article: <a href="http://www.iandevlin.com/blog/2011/04/html5/html5-section-or-article">section or article?</a> might help you choose.</p>
+            
+        </aside>
 
 <?php
     // Renders this page's content in the variable below
