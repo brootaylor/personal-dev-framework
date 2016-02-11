e17ceab1fff30ab8fb1de0dfd6f6d6f297083446
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-02-11 11:24:07 +0000

    Tidy up and added links to accessibility and cookies pages.

--- application/php_partials/_footer.php
+++ application/php_partials/_footer.php
@@ -2,7 +2,15 @@
         <!-- ARIA: the landmark role "contentinfo" is added here as it contains metadata that applies to the parent document -->
         <footer role="contentinfo" id="footer">
             <!-- Copyright information can be contained within the <small> element. The <time> element is used here to indicate that the '2015' is a date -->
-            <small>Copyright &copy; 2002&ndash;<time datetime="<?php echo date("Y") ?>"><?php echo date("Y") ?></time> Bruce Taylor</small>
+            <small>Copyright &copy; 2002&ndash;<time datetime="<?php echo date("Y") ?>"><?php echo date("Y") ?></time> Bruce Taylor. All rights reserved.</small>
+            <ul>
+                <li>
+                    <a href="//<?php echo $server; ?>/accessibility">Accessibility</a>
+                </li>
+                <li>
+                    <a href="//<?php echo $server; ?>/cookies">Cookies</a>
+                </li>
+            </ul>
         </footer>
         <!-- / Footer content -->
 
@@ -23,12 +31,12 @@
         <!-- build:js //ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.js"></script>
         <!-- /build -->
-        <script>window.jQuery || document.write('<script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="//<?php echo $server; ?>/static/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
 
         <!-- All plugin scripts eg. jQuery plugins and other 3rd party scripts -->
-        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/plugins.js"></script>
+        <script src="//<?php echo $server; ?>/static/js/plugins.js"></script>
         <!-- Site-specific JS -->
-        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/main.js"></script>
+        <script src="//<?php echo $server; ?>/static/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
