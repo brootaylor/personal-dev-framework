c44ddad4844c6183441e74ed7f40332f27f331e9
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-03-13 20:58:05 +0000

    Added grid markup.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     $page_title = "This is the page title";
     $page_title_extra = "| The title's context eg. | Bruce Taylor's Personal Website";
     $page_description = "This will render in the meta description tag";
-    $page_canonical = "/Page URL goes here for canonical purposes. Not needed on index page. eg. /about-us";
+    $page_canonical = "/";
 
     ob_start();
 ?>
@@ -14,9 +14,34 @@
         <main> element until user agents implement the required role mapping. -->
         <main role="main" id="main">
 
-            <h1><?php echo $page_title; ?></h1>
+            <article>
 
-            <p>Enim turpis enim vel! Parturient mus lectus parturient nec sagittis parturient! Enim in auctor in. In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat. Adipiscing aenean es.</p>
+                <h1><?php echo $page_title; ?></h1>
+
+                <p>Enim turpis enim vel! Parturient mus lectus parturient nec sagittis parturient! Enim in auctor in. In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat. Adipiscing aenean es.</p>
+
+                <hr>
+
+                <h2>Grid example</h2>
+
+                <div class="grid">
+                    <div class="col">
+                        <p>In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat.</p>
+                    </div>
+                    <div class="col">
+                        <p>In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat.</p>
+                    </div>
+                    <div class="col">
+                        <p>In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat.</p>
+                    </div>
+                    <div class="col">
+                        <p>In platea urna, turpis in. Porttitor dictumst. Massa elementum habitasse eros cursus, nunc, natoque porta mus pellentesque, proin tortor, integer lundium ac mauris integer risus, a non elit nunc lundium, quis dapibus risus et! Porta, mus etiam, lorem nascetur nec urna penatibus placerat.</p>
+                    </div>
+                </div>
+
+                <hr>
+
+            </article>
             
         </main>
         <!-- / Main content -->
