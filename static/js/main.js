b994356a394e98307c791ba0dea17c0fc37c163e
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-03-13 21:01:31 +0000

    File outputs.

--- static/js/main.js
+++ static/js/main.js
@@ -7,16 +7,27 @@
 // For larger projects, you can make use of a JavaScript module loader, like Require.js, to load any other scripts you need to run.
 
 
-// Hamburger icon toggling
-/////////////////////////////
+// Hamburger icon toggling & menu show / hide
+//////////////////////////////////////////////
 
-$(function() {
-    var menuButton = document.getElementById('menuButton');
-    menuButton.addEventListener('click', function (e) {
-        menuButton.classList.toggle('is-active');
-        e.preventDefault();
-    });
-});
+(function() {
+    var menuButton = document.querySelector('#menuButton'),
+    	nav = document.querySelector("#nav ul");
+
+    if (menuButton) {
+    	menuButton.addEventListener('click', function (e) {
+	        menuButton.classList.toggle('is-active');
+
+	        if (nav.className === "open") {
+	        	nav.className = "";
+	        } else {
+	        	nav.className = "open";
+	        }
+
+	        e.preventDefault();
+	    }, false);
+    }
+})();
 
 ///////////////////////////////////////////////////////////////
 
