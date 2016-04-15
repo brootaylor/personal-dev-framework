c23b9be46225fb3a2bd083b3588aa29fad96f162
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-04-15 17:23:04 +0100

    Upgraded to latest 1.x version of jQuery.

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,7 +193,7 @@ module.exports = function (grunt) {
 		        	'<%= dirs.cssBuild %>/styles.css': '<%= dirs.css %>/styles.css',
 
 		        	// Javascript library files...
-		        	'<%= dirs.jsBuild %>/vendor/jquery-1.12.0.min.js': '<%= dirs.js %>/vendor/jquery-1.12.0.min.js',
+		        	'<%= dirs.jsBuild %>/vendor/jquery-1.12.3.min.js': '<%= dirs.js %>/vendor/jquery-1.12.3.min.js',
                     '<%= dirs.jsBuild %>/vendor/html5shiv.js': '<%= dirs.js %>/vendor/html5shiv.js',
 		        	'<%= dirs.jsBuild %>/vendor/modernizr.custom.72511.js': '<%= dirs.js %>/vendor/modernizr.custom.72511.js',
 
