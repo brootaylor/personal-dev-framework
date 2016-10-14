a87a3f4ba556bfc41194114cb4c92095d5564d99
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-10-14 23:18:53 +0100

    'Sticky' CSS version comments added to beginning of CSS

--- build/application/php_partials/_footer.php
+++ build/application/php_partials/_footer.php
@@ -27,9 +27,9 @@
         <script>window.jQuery || document.write('<script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/vendor/jquery-1.12.2.min.js"><\/script>')</script>
 
         <!-- All plugin scripts eg. jQuery plugins and other 3rd party scripts -->
-        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/plugins.1467128324506.js"></script>
+        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/plugins.1476483440915.js"></script>
         <!-- Site-specific JS -->
-        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/main.1467128324506.js"></script>
+        <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/static/js/main.1476483440915.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
