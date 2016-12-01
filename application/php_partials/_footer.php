8f474f798aee784435a298bb3a85735ee3d6bd4b
Bruce Taylor <bwtaylor@Bs-MacBook-Pro.local>
2016-12-01 12:51:50 +0000

    Service worker placeholder files added

--- application/php_partials/_footer.php
+++ application/php_partials/_footer.php
@@ -35,6 +35,13 @@
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
+            // Service Worker
+            if('serviceWorker' in navigator) {
+                navigator.serviceWorker
+                    .register('static/js/sw.js')
+                    .then(function() { console.log("Service Worker Registered"); });
+            }
+
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
